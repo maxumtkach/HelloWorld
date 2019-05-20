0234f179ab695fe45517fbfbef2469852ddcc8e6
maxumtkach <makstkach@bigmir.net>
2019-05-20 09:11:10 +0300

    //1.1.2 Собственный Hello world

---
+++ app/src/main/java/com/example/helloworld/SplashScreenActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+    }
+}
